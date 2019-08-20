de1bcb6dec1bbf4c289869f62407e02411cd3ca8
Zac Steinberg <zstein1234@gmail.com>
2019-08-20 17:03:01 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data[:gender].each { |attribute|
+    attribute[1].each { |name|
+      pigeon_list[name] = {
+        :color => [],
+        :gender => [],
+        :lives => []
+      }
+    }
+  }
+  
+  data.each { |data|
+    data[1].each { |item|
+      item[1].each { |pigeon|
+        if pigeon_list.keys.include?(pigeon)
+          pigeon_list[pigeon][data[0]].push(item[0].to_s)
+        end
+      }
+    }
+  }
+  
+  pigeon_list
+  
 end
