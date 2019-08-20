f50ca331db9dbe3c471c4937823b90bd844647de
Zac Steinberg <zstein1234@gmail.com>
2019-08-20 16:10:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data[:gender].each { |attribute|
+    attribute[1].each { |name|
+      pigeon_list[name] = {
+        :color => [],
+        :gender => [],
+        :lives => []
+      }
+    }
+  }
+  
+  #iterate over data hash grabbing each attribute
+   # iterate over each item in attribute
+    #  check if item includes name and add item to name in pigeon_list
+  
+  pigeon_list
+  
 end
